18e4409b56f9f868f1f876cb3efd076ce09c291b
mahesh maurya <mahesh@wohlig.com>
2016-05-04 12:49:07 +0530

    home slider done

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,31 +1,47 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['starter.services'])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
-})
-.controller('LustCtrl', function($scope) {
+  })
+  .controller('LustCtrl', function($scope) {
 
-})
-.controller('LoveCtrl', function($scope) {
+  })
+  .controller('LoveCtrl', function($scope) {
 
-})
-.controller('LieCtrl', function($scope) {
+  })
+  .controller('LieCtrl', function($scope) {
 
-})
-.controller('GoopsCtrl', function($scope) {
+  })
+  .controller('GoopsCtrl', function($scope) {
 
-})
-.controller('SexpertCtrl', function($scope) {
+  })
+  .controller('SexpertCtrl', function($scope) {
 
-})
-.controller('ConfessionsCtrl', function($scope) {
+  })
+  .controller('ConfessionsCtrl', function($scope) {
 
-})
-.controller('LoveQuieryCtrl', function($scope) {
+  })
+  .controller('LoveQuieryCtrl', function($scope) {
 
-})
-.controller('DetailCtrl', function($scope) {
+  })
+  .controller('DetailCtrl', function($scope) {
 
-})
-.controller('HomeCtrl', function($scope, $stateParams) {
-});
+  })
+  .controller('HomeCtrl', function($scope, $stateParams, MyServices, $timeout) {
+    $scope.allCategory = [
+      []
+    ];
+
+    function getArticles(data) {
+      console.log(data.category.id);
+      _.each(data.article, function(n) {
+        n.timestamp2 = moment(n.timestamp).toDate();
+      });
+      $scope.allCategory[parseInt(data.category.id)] = data.article;
+    }
+
+    for (var i = 0; i < 7; i++) {
+      $scope.allCategory.push([]);
+      MyServices.getCategoryArticles(i + 1, getArticles);
+    }
+  });
